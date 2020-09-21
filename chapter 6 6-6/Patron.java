180463db531df7c7745f1e57211d755541814739
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-21 06:13:20 -0700

    Add files via upload

---
+++ chapter 6 6-6/Patron.java
@@ -0,0 +1,9 @@
+public class Patron {
+    private String name;
+    private int bookCount;
+
+    public Patron(String patronName, int numberOfBooks) {
+        name = patronName;
+        bookCount = numberOfBooks;
+    }
+}
