c8c0e7b89467ae64d899415095d660a5e7dee471
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-21 14:01:29 -0700

    Add files via upload

--- chapter 6/6-6/Patron.java
+++ chapter 6/6-6/Patron.java
@@ -1,10 +1,45 @@
 public class Patron {
     private String name;
-    // private int bookCount;
+    private String book1;
+    private String book2;
+    private String book3;
 
-    public Patron(String patronName, /*int numberOfBooks*/) {
+    public Patron(String patronName) {
         name = patronName;
-        // bookCount = numberOfBooks;
+        book1 = null;
+        book2 = null;
+        book3 = null;
+    }
+/*
+    public boolean borrowBook(String title, String book) {
+        if (book1 == null) {
+            book1 = book;
+        } else if (book2 == null) {
+            book2 = book;
+        } else if (book3 = null) {
+            book3 = book;
+        } else {
+            return false;
+        }
+    } */
+
+    public boolean isBorrowed(String title) {
+        if (title.equals(book1) || title.equals(book2) || title.equals(book3)) {
+            return true;
+        } else { 
+            return false;
+        }
+    }
+
+    // logic error: if title does not match any of the three then book3 is erased
+    public void returnBook(String title) {
+        if (title.equals(book1)) {
+            book1 = null;
+        } else if (title.equals(book2)) {
+            book2 = null;
+        } else {
+            book3 = null;
+        }
     }
 
     
