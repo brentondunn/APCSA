ce64b6fe4a3acfa3e375ac1ef69fe83db0d5c66a
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-23 17:39:14 -0700

    Update Fraction.java

--- chapter 6/6-3/Fraction.java
+++ chapter 6/6-3/Fraction.java
@@ -6,22 +6,27 @@ date of completion: 24/08/2020
 
 public class Fraction {
 
+    // instance variables 
     private int numerator;
     private int denominator;
 
+    // constructor method 
     public Fraction(int n, int d) {
         numerator = n;
         denominator = d;
     }
 
+    // accessor method 
     public int getNumerator() {
         return numerator;
     }
-
+    
+    // accessor method
     public int getDenominator() {
         return denominator;
     }
 
+    // method that adds two fractions
     public String add(Fraction fraction2) {
         int top = (numerator * fraction2.getDenominator()) + (fraction2.getNumerator() * denominator);
         int bottom = denominator * fraction2.getDenominator();
@@ -29,7 +34,8 @@ public class Fraction {
         String strBottom = Integer.toString(bottom);
         return strTop + '/' + strBottom;
     }
-
+    
+    // method that subtracts two fractions
     public String subtract(Fraction fraction2) {
         int top = (numerator * fraction2.getDenominator()) - (fraction2.getNumerator() * denominator);
         int bottom = denominator * fraction2.getDenominator();
@@ -38,6 +44,7 @@ public class Fraction {
         return strTop + '/' + strBottom;
     }
 
+    // method that multiplies two fractions
     public String multiply(Fraction fraction2) {
         int top = numerator * fraction2.getNumerator();
         int bottom = denominator * fraction2.getDenominator();
@@ -46,6 +53,7 @@ public class Fraction {
         return strTop + '/' + strBottom;
     }
 
+    // method that divides two fractions
     public String divide(Fraction fraction2) {
         int top = numerator * fraction2.getDenominator();
         int bottom = denominator * fraction2.getNumerator();
