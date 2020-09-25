a8522a2523a42bc8474b7719758e5b3dc8fdb76f
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-24 18:53:16 -0700

    Add files via upload

--- chapter 6/6-3/Main.java
+++ chapter 6/6-3/Main.java
@@ -1,7 +1,17 @@
-public Main {
+/*
+name: Brenton Dunn and Aman Kumar
+ID #: 218005 and 218xxx
+date of completion: 24/08/2020
+*/
+
+public class Main {
   public static void main(String[] args) {
-    Fraction f = new Fraction(1, 2);
-    Fraction g = new Fraction(2, 3);
+
+    // instantiate fractions
+    Fraction f = new Fraction(3, 4);
+    Fraction g = new Fraction(2, 7);
+
+    // add, subtract, multiply, and divide fractions
     System.out.println(f.add(g));
     System.out.println(f.subtract(g));
     System.out.println(f.multiply(g));
