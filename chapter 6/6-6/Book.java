97e3c477cc05c4d98ba09eda8516982e6bbc8ace
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-24 18:21:33 -0700

    Update Book.java

--- chapter 6/6-6/Book.java
+++ chapter 6/6-6/Book.java
@@ -1,16 +1,21 @@
 public class Book {
+    
+    // instance variables
     private String title;
     private String author;
 
+    // constructor method
     public Book(String bookTitle, String bookAuthor) {
         title = bookTitle;
         author = bookAuthor;
     }
 
+    // accessor method
     public String getTitle() {
         return title;
     }
 
+    // accessor method   
     public String getAuthor() {
         return author;
     }
