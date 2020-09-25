9f0a968f923743575742f8e9f833653c03c7d345
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-24 18:38:55 -0700

    Add files via upload

--- chapter 6/6-6/Main.java
+++ chapter 6/6-6/Main.java
@@ -1,22 +1,32 @@
-/*
-name: Brenton Dunn and Aman Kumar
-ID #: 218005 and 218059
-date of completion: 24/08/2020
-*/
-
 public class Main {
     public static void main(String[] args) {
+
+        // instantiate patron
         Patron p = new Patron("John");
-        p.borrowBook("Title Hello");
-        p.borrowBook("Title Hello2");
-        p.borrowBook("Title Hello3");
-        p.borrowBook("Title Hello4");
+        
+        // patron borrows books
+        p.borrowBook("Title1");
+        p.borrowBook("Title2");
+        p.borrowBook("Title3");
+        p.borrowBook("Title4");
 
-        boolean x = p.isBorrowed("Title Hello4");
-        System.out.println("is borroed: " + x);
-        p.returnBook("Title Hello");
-        boolean y = p.isBorrowed("Title Hello");
-        System.out.println("is borrowed part 2: " + y);
+        // asks whether it has borrowed a book
+        System.out.println("Is Title2 borrowed? " + p.isBorrowed("Title2"));
+        System.out.println("Is Title4 borrowed? " + p.isBorrowed("Title4") + " (becuase it was over three books)");
+        
+        // patron returns a book
+        p.returnBook("Title2");
 
+        // checks to see if book has been returned
+        System.out.println("Title2 has been returned. (double check) Is Title2 still checked out? " + p.isBorrowed("Title Hello"));
+
+        //instantiates a new book
+        Book b = new Book("Green Book", "Dan");
+
+        // examines a book's title
+        System.out.println("The title of the book is " + b.getTitle() + ".");
+
+        // examines a book's author
+        System.out.println("The author of the book is " + b.getAuthor() + ".");
     }
 }
