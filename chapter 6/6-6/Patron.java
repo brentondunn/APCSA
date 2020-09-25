776b6a01e2c31c46c017394a747776ae083f3dff
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-09-24 18:25:13 -0700

    Update Patron.java

--- chapter 6/6-6/Patron.java
+++ chapter 6/6-6/Patron.java
@@ -1,9 +1,18 @@
+/*
+name: Brenton Dunn and Aman Kumar
+ID #: 218005 and 218059
+date of completion: 24/08/2020
+*/
+
 public class Patron {
+    
+    // instance variables
     private String name;
     private String book1;
     private String book2;
     private String book3;
 
+    // constructor method
     public Patron(String patronName) {
         name = patronName;
         book1 = null;
@@ -11,6 +20,7 @@ public class Patron {
         book3 = null;
     }
 
+    // method that assigns a title to a book spot. if all spots are filled, it returns false
     public boolean borrowBook(String title) {
         if (book1 == null) {
             book1 = title;
@@ -26,6 +36,7 @@ public class Patron {
         }
     } 
 
+    // method that checks the given title with the titles that are borrowed
     public boolean isBorrowed(String title) {
         if (title.equals(book1) || title.equals(book2) || title.equals(book3)) {
             return true;
@@ -34,6 +45,7 @@ public class Patron {
         }
     }
 
+    // method that returns a specified title
     public void returnBook(String title) {
         if (title.equals(book1)) {
             book1 = null;
@@ -45,7 +57,4 @@ public class Patron {
             System.out.println("Title is not borrowed.");
         }
     }
-
-    
-
 }
