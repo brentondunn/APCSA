e84c31f654aef77dbfeb222b693fbccab850dafa
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-10-23 15:52:21 -0700

    Create Pi.java

---
+++ Selected Algorithms Lab/Leibniz/Pi.java
@@ -0,0 +1,35 @@
+// Brenton Dunn
+// 218005
+// 29 October 2020
+
+import java.util.Scanner;
+
+public class Pi {
+    public static void main(String[] args) {
+
+        int denominator = 1;
+        double finalNum = 0;
+
+        // get user input on # of iterations
+        Scanner number = new Scanner(System.in);
+        System.out.println("How many iterations would you like? ");
+        int iterations = number.nextInt();
+        
+        // iterates the specified # of times
+        for (int i = 0; i < iterations; i++) {
+            
+            // decides whether to add or subtract the number by looking at the number of times it has already iterated 
+            if (i % 2 == 0) {
+                finalNum += 1 / (double) denominator;
+            } else {
+                finalNum -= 1/ (double) denominator;
+            }
+
+            // prints the numbere each iteration
+            System.out.println(finalNum * 4);
+
+            // adds 2 to the denominator each iteration
+            denominator += 2;
+        }        
+    }
+}
