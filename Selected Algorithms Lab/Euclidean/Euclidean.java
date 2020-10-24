701b248c824009cd0285e1f543935a1ca41ef4b6
brentonmdunn <47510212+brentonmdunn@users.noreply.github.com>
2020-10-23 19:32:14 -0700

    Update Euclidean.java

--- Selected Algorithms Lab/Euclidean/Euclidean.java
+++ Selected Algorithms Lab/Euclidean/Euclidean.java
@@ -7,24 +7,34 @@ import java.util.Scanner;
 public class Euclidean {
     public static void main(String[] args) {
         
-        int temp;
+        int remainder;
 
+        // get user input for two numbers
         Scanner scan = new Scanner(System.in);
-        System.out.println("Number1: ");
+        System.out.print("Number 1: ");
         int num1 = scan.nextInt();
-        System.out.println("Number2: ");
+        System.out.print("Number 2: ");
         int num2 = scan.nextInt();
         
+        // loops while neither num1 nor num2 == 0
         while (num1 != 0 && num2 != 0) {
+            
+            // checks to see which number is larger. The larger number becomes the dividend
             if (num1 > num2) {
-                temp = num1 % num2;
+                remainder = num1 % num2;
             } else {
-                temp = num2 % num1;
+                remainder = num2 % num1;
             }
 
+            // num2 turns into num1 and num2 stores the remainder 
             num1 = num2;
-            num2 = temp;
+            num2 = remainder;
+
+            // prints the numbers each iteration
+            System.out.println("\nThe numbers at this time are " + num1 + " and " + num2 + ".");
         }
-        System.out.println("The GCF is " + num1);
+
+        //final output
+        System.out.println("The GCF is " + num1 + ".");
     }
 }
